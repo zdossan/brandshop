cbc65768af2eb9bbc3f47d5c38b146a0fb73f775
zdossan <zhanerke.dossan@nu.edu.kz>
2025-09-14 15:30:57 +0500

    created a project

---
+++ scripts/brandshop.js
@@ -0,0 +1,35 @@
+const products = [
+	{
+	image: 'assets/products/sweatshirt_nu.jpg',
+	name: 'Свитшот',
+	rating:{
+		stars:4.5,
+		count:87
+	},
+	priceCents: 1090
+},
+{
+	image: 'clampBrown.jpg',
+	name: 'Зажим, аксессуар для одежды',
+	rating:{
+		stars:4,
+		count:127
+	},
+	priceCents: 650
+},{
+	image: 'clampBrown.jpg',
+	name: 'Зажим, аксессуар для одежды',
+	rating:{
+		stars:4,
+		count:127
+	},
+	priceCents: 650
+},{
+	image: 'polo.jpg',
+	name: 'Поло',
+	rating:{
+		stars:4.5,
+		count:56
+	},
+	priceCents: 1200
+}];
