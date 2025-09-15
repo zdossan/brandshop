03ce3da36684a078d3593da051cac9ed1d148fe6
zdossan <zhanerke.dossan@nu.edu.kz>
2025-09-15 18:04:52 +0500

    added separate files for products

---
+++ data/products.js
@@ -0,0 +1,35 @@
+const products = [
+	{
+	image: 'assets/products/sweatshirt_nu.jpg',
+	name: 'Свитшот',
+	rating:{
+		stars:4.5,
+		count:87
+	},
+	priceCents: 1090
+},
+{
+	image: 'assets/products/clampBrown.jpg',
+	name: 'Зажим, аксессуар для одежды',
+	rating:{
+		stars:4,
+		count:127
+	},
+	priceCents: 650
+},{
+	image: 'assets/products/polo.jpg',
+	name: 'Поло',
+	rating:{
+		stars:4.5,
+		count:56
+	},
+	priceCents: 1200
+},{
+  image:'assets/products/costume.jpg',
+  name:'Костюм',
+  rating:{
+    stars:5,
+    count:4
+  },
+  priceCents: 5600
+}];
