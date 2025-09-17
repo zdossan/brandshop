ae06901e3a0fe1f40cc7bbfe0a43eb0d9542112e
zdossan <zhanerke.dossan@nu.edu.kz>
2025-09-17 14:13:56 +0500

    Enables add to cart button and added style

--- scripts/brandshop.js
+++ scripts/brandshop.js
@@ -45,7 +45,7 @@ products.forEach(function(product){
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">
             Add to Cart
           </button>
         </div>`;
@@ -53,6 +53,29 @@ products.forEach(function(product){
 	
 })
 
-console.log(productsHTML);
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button)=>{
+    button.addEventListener('click',()=>{
+      const productName = button.dataset.productName;
+
+      let matchingItem;
+
+      cart.forEach((item)=>{
+        if(productName === item.productName){
+          matchingItem = item;
+        }
+      })
+      if(matchingItem){
+        matchingItem.quantity += 1;
+      }else{
+        cart.push({
+        productName:productName,
+        quantity:1,
+      });
+      }
+      console.log(cart);
+    })
+  })
