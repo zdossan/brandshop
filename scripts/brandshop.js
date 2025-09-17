71d44871d4739087fcd8c9350e41811404906df9
zdossan <zhanerke.dossan@nu.edu.kz>
2025-09-17 14:28:13 +0500

    made add to cart interactive

--- scripts/brandshop.js
+++ scripts/brandshop.js
@@ -45,7 +45,7 @@ products.forEach(function(product){
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>`;
@@ -59,12 +59,12 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button)=>{
     button.addEventListener('click',()=>{
-      const productName = button.dataset.productName;
+      const productId = button.dataset.productId;
 
       let matchingItem;
 
       cart.forEach((item)=>{
-        if(productName === item.productName){
+        if(productId === item.productId){
           matchingItem = item;
         }
       })
@@ -72,7 +72,7 @@ document.querySelectorAll('.js-add-to-cart')
         matchingItem.quantity += 1;
       }else{
         cart.push({
-        productName:productName,
+        productId:productId,
         quantity:1,
       });
       }
